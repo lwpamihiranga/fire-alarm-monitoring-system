589b49155f13136a569394c38334a962eb623e8c
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-22 03:45:27 +0530

    updated with middlewares

--- rest-api/src/server.js
+++ rest-api/src/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const sensorRouter = require('./resources/sensor/sensor.router');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/firealarm_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('working');
 });
+
+app.use('/sensor', sensorRouter);
 
 app.listen(3000, () => {
   console.log('REST API started to listen on port 3000');
