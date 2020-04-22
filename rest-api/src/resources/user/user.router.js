b98c8320053281850d60af7ce3f5a8409b1d194f
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-22 22:59:42 +0530

    login implemented with jwt token

--- rest-api/src/resources/user/user.router.js
+++ rest-api/src/resources/user/user.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('./user.model');
 
@@ -43,6 +44,52 @@ router.post('/signup', (req, res) => {
           }
         });
       }
+    });
+});
+
+router.post('/login', (req, res) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: 'Auth failed',
+        });
+      }
+
+      bcrypt.compare(req.body.password, user[0].password, (error, result) => {
+        if (error) {
+          return res.status(401).json({
+            message: 'Auth failed',
+          });
+        }
+
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            'secret',
+            {
+              expiresIn: '1h',
+            }
+          );
+          return res.status(200).json({
+            message: 'Authentication successfull',
+            token: token,
+          });
+        }
+
+        res.status(401).json({
+          message: 'Auth failed',
+        });
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: error,
+      });
     });
 });
 
