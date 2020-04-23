bee840072b88155b71bf1eedafda32005c29d55f
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-24 01:30:00 +0530

    sensor-status component created

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SensorStatusComponent } from './shared/components/sensor-status/sensor-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SensorStatusComponent
   ],
   imports: [
     BrowserModule,
---
+++ web-client/src/app/shared/components/sensor-status/sensor-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sensor-status',
+  templateUrl: './sensor-status.component.html',
+  styleUrls: ['./sensor-status.component.css']
+})
+export class SensorStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
