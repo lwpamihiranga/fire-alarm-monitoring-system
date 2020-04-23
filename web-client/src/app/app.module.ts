8341f43f7480e7af9bc1073c08b0756d40c5748a
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-24 01:43:11 +0530

    HttpClientModule imported

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SensorStatusComponent } from './shared/components/sensor-status/sensor-status.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SensorStatusComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SensorStatusComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
