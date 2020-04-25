f30c9d8e52ef1bf311423f4b04fb43a7e3f6027a
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-25 13:04:27 +0530

    header component created

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SensorListComponent } from './shared/components/sensor-list/sensor-list.component';
 import { SensorComponent } from './shared/components/sensor/sensor.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, SensorListComponent, SensorComponent],
+  declarations: [AppComponent, SensorListComponent, SensorComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
