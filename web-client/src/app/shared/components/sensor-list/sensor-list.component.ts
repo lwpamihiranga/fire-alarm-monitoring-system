a1fb843357871ad231fd25f08831e476e27979cb
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-25 17:16:50 +0530

    timer added to run api call every 40 seconds

--- web-client/src/app/shared/components/sensor-list/sensor-list.component.ts
+++ web-client/src/app/shared/components/sensor-list/sensor-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SensorDataService } from '../../services/sensor-data.service';
 import { Sensor } from '../../interfaces/sensor';
-
+import { Observable, timer, Subject } from 'rxjs';
+import { takeUntil, switchMap, catchError } from 'rxjs/operators';
 @Component({
   selector: 'app-sensor-list',
   templateUrl: './sensor-list.component.html',
@@ -10,13 +11,27 @@ import { Sensor } from '../../interfaces/sensor';
 export class SensorListComponent implements OnInit {
   sensors: Sensor[];
 
-  constructor(private sensorDataService: SensorDataService) {
-    this.sensors = [];
-  }
+  constructor(private sensorDataService: SensorDataService) {}
 
-  ngOnInit(): void {
-    this.sensorDataService.getSensorsData().subscribe((data) => {
+  ngOnInit(): void {}
+
+  private killTrigger: Subject<void> = new Subject();
+
+  private fetchData: Observable<
+    Sensor[]
+  > = this.sensorDataService.getSensorsData();
+
+  private refrestInterval = timer(0, 40000)
+    .pipe(
+      takeUntil(this.killTrigger),
+      switchMap(() => this.fetchData)
+      // catchError(error => 'Error')
+    )
+    .subscribe((data) => {
       this.sensors = data;
     });
+
+  ngOnDestroy() {
+    this.killTrigger.next();
   }
 }
