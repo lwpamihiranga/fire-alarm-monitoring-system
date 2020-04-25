4ac4da79b7e18585a5445c0ecea537fdc470cf30
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-25 09:26:14 +0530

    sensor data service injected

--- web-client/src/app/shared/components/sensor-list/sensor-list.component.ts
+++ web-client/src/app/shared/components/sensor-list/sensor-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { SensorDataService } from '../../services/sensor-data.service';
+import { Sensor } from '../../interfaces/sensor';
 
 @Component({
   selector: 'app-sensor-list',
   templateUrl: './sensor-list.component.html',
-  styleUrls: ['./sensor-list.component.css']
+  styleUrls: ['./sensor-list.component.css'],
 })
 export class SensorListComponent implements OnInit {
+  private sensors: Sensor[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private sensorDataService: SensorDataService) {
+    this.sensors = [];
   }
 
+  ngOnInit(): void {
+    this.sensorDataService.getSensorsData().subscribe((data) => {
+      this.sensors = data;
+    });
+  }
 }
