5d55fd764435a1aa9bc86d6846dbe4df1a4c6e22
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-25 09:23:34 +0530

    sensor component created

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SensorListComponent } from './shared/components/sensor-list/sensor-list.component';
+import { SensorComponent } from './shared/components/sensor/sensor.component';
 
 @NgModule({
-  declarations: [AppComponent, SensorListComponent],
+  declarations: [AppComponent, SensorListComponent, SensorComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
