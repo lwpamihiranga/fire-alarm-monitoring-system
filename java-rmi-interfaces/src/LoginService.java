2e740dac83c90e554020de6de61a8078c0f8b13f
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-03 11:04:32 +0530

    login service interface updated

--- java-rmi-interfaces/src/LoginService.java
+++ java-rmi-interfaces/src/LoginService.java
@@ -2,5 +2,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface LoginService extends Remote{
-    void loginAsAdmin() throws RemoteException;
+    String loginAsAdmin() throws RemoteException;
 }
