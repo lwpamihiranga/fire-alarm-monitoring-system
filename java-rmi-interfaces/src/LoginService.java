8865056fe192c58e4283aa7e920048e8ca2a8143
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-03 16:10:51 +0530

    rmi projects removed due to failure

--- java-rmi-interfaces/src/LoginService.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface LoginService extends Remote{
-    String loginAsAdmin() throws RemoteException;
-}
--- java-rmi-interfaces/src/SensorDataService.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface SensorDataService extends Remote {
-    String getSensorData() throws RemoteException;
-    
-    Boolean addSensor() throws RemoteException;
-}
