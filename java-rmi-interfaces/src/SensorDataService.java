93395ebfb95d9fa2948c63745ffd840e5c05e19e
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-04 20:48:55 +0530

    input parameter types changed

--- java-rmi-interfaces/src/SensorDataService.java
+++ java-rmi-interfaces/src/SensorDataService.java
@@ -4,5 +4,5 @@ import java.rmi.RemoteException;
 public interface SensorDataService extends Remote {
     String getSensorData() throws RemoteException;
     
-    Boolean addSensor(String roomNo, String floor, String token) throws RemoteException;
+    Boolean addSensor(int roomNo, int floor, String token) throws RemoteException;
 }
