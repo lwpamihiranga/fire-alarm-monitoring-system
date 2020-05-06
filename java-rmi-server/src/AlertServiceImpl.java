7cd8194612b780c30462d37e8889b0bc8ab372ec
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-06 08:40:00 +0530

    alert service implementation

---
+++ java-rmi-server/src/AlertServiceImpl.java
@@ -0,0 +1,12 @@
+
+public class AlertServiceImpl implements AlertService {
+    @Override
+    public void sendEmailAlert(String sensorId) {
+        System.out.println("Sending email alert on sensor " + sensorId);
+    }
+    
+    @Override
+    public void sendSMSAlert(String sensorId) {
+        System.out.println("Sending SMS alert on sensor " + sensorId);
+    }
+}
